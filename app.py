1839e05a740284866e8a2a2deab393e3f1be660a
nkeff <48253830+nkeff@users.noreply.github.com>
2022-02-11 12:03:36 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import requests
 from config import Config
 
 config = Config()
+config.update()
 
 url = f'{config.base_url}instance{config.instance}/sendMessage?token={config.token}'
 
 response = requests.request("POST", url, headers=config.headers, data=config.payload)
+
+print(response.json())
--- get_qr.py
+++ get_qr.py
@@ -2,6 +2,7 @@ from base64 import b64decode
 import json
 import time
 import requests
+import os
 
 from config import Config
 
@@ -9,8 +10,7 @@ config = Config()
 
 
 requests_counter = 0
-while True:
-
+while 0:
     # Ищем свободный чат
     get_chat_url = f"{config.base_url}chat/spare?crm=TEST&domain=test"
     get_chat_response = requests.request("GET", get_chat_url, headers=config.headers)
@@ -19,8 +19,10 @@ while True:
     # обработка ошибок
     if get_chat_response.status_code == 200 and not get_chat_response.json().get('error_code'):
         config.save_chat_info(json.dumps(get_chat_response.json(), indent=1))
+        print("chat info was Saved!")
+        break
     else:
-        if requests_counter > 20:
+        if requests_counter > 30:
             print("timeout error")
             exit()
 
@@ -28,20 +30,24 @@ while True:
         print(get_chat_response.json())
         time.sleep(5)
         continue
-    
 
-    requests_counter = 0
+config.update()
+
+requests_counter = 0
+while True:
+
     # Инициализируем чат
     init_chat_url = f"{config.base_url}instance{config.instance}/status?token={config.token}"
     init_chat_response = requests.request("GET", init_chat_url, params=config.params, headers=config.headers)
 
-    print('counter:', requests_counter, '| status:', init_chat_response.json().get('state'))
+    print('counter:', requests_counter, '| state:', init_chat_response.json().get('state'))
     
     if init_chat_response.json().get('state') == 'got qr code':
-        config.save_qr_code(init_chat_response.get('qrCode')) 
+        config.save_qr_response(init_chat_response.json().get('qrCode')) 
+        break
 
     requests_counter += 1
-    if requests_counter > 10:
+    if requests_counter > 20:
         print("timeout error")
         exit()
     time.sleep(5)
